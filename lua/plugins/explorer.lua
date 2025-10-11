e833204e1b8f8fff78fa283583bcb621f3d224aa
kingpoem <1322153392@qq.com>
2025-10-11 13:18:11 +0800

    fix: fix 'L' in neo-tree will quit bug

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -15,7 +15,7 @@ local NeoTree = {
 
     config = function()
         require('neo-tree').setup {
-            close_if_last_window = true,
+            close_if_last_window = false,
             enable_git_status = true,
             enable_diagnostics = true,
 
