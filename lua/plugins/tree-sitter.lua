cc6ea382fc7517465d385ab2ca82ef6db0ae5c0c
kingpoem <1322153392@qq.com>
2025-10-11 14:45:17 +0800

    feat: add more language in tree-sitter

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -13,26 +13,37 @@ local TreeSitter = {
         require('nvim-treesitter.configs').setup {
             -- A list of parser names, or "all" (the listed parsers MUST always be installed)
             ensure_installed = {
-                'c',
-                'lua',
-                'vim',
-                'cpp',
-                'python',
-                'json',
-                'yaml',
-                'toml',
-                'bash',
-                'fish',
                 'awk',
-                'diff',
+                'bash',
+                'c',
                 'cmake',
+                'cpp',
+                'css',
+                'csv',
+                'dart',
+                'diff',
+                'fish',
+                'html',
+                'java',
+                'json',
+                'kotlin',
+                'lua',
+                'nix',
+                'python',
+                'swift',
+                'toml',
+                'verilog',
+                'vim',
+                'yaml',
+                'zig',
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
 
             -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+            -- WARN: set to false if you don't have `tree-sitter` CLI installed locally
+            -- on MacOS: brew install tree-sitter-cli
             auto_install = true,
 
             -- List of parsers to ignore installing (or "all")
