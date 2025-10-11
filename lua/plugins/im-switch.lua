b128ce64c991bddf77d8a179a762e17501ec0a9d
kingpoem <1322153392@qq.com>
2025-10-11 13:28:57 +0800

    delete: delete im-switch
    自动切换输入法插件引入了更多的问题，且原本在各个应用之间切换时转换输入法已习以为常

--- lua/plugins/im-switch.lua
+++
@@ -1,7 +0,0 @@
--- IME auto switcher
-
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
-local Fcitx5 = { 'www9song/fcitx5-nvim-zh', event = { 'BufReadPost', 'BufNewFile' } }
-
-return Fcitx5
